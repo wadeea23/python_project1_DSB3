d25f7acc10676993b50eef7e9bff948747ddc80f
wadeea23 <almutaweaa@gmail.com>
2026-09-21 16:51:54 +0300

    Add files via upload

---
+++ Habit_Tracker_app.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+import datetime
+import streamlit as st
+import utils
+import requests
+from openai import OpenAI
+
+st.title("Habit Tracker")
+
+video_file = open("Habit_Tracker.mp4", "rb")
+video_bytes = video_file.read()
+
+st.video(video_bytes)
